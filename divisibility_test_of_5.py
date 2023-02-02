29159512969bf5d4527996cf0299e5908c9ed651
Lotini B. Victor <122792098+victorlotini@users.noreply.github.com>
2023-02-02 11:55:25 +0300

    Add files via upload

---
+++ divisibility_test_of_5.py
@@ -0,0 +1,6 @@
+# a program to check if a number is divisible by 5
+num = int(input("enter a number"))
+if num % 5 == 0:
+    print("Divisible by 5")
+else:
+    print("The number is not divisible by 5")
